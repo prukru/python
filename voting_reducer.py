ca06cd250cd10ed9a5818f8cea9f01c938d70fb5
prukru <68339463+prukru@users.noreply.github.com>
2020-07-22 18:24:26 -0400

    Create voting_reducer.py

---
+++ voting_reducer.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+
+import sys
+
+last_Candidate = None
+Vote_count = 0
+
+for line in sys.stdin:
+
+    line = line.strip()
+    Candidate, count = line.split("\t")
+
+    count = int(count)
+    # if this is the first iteration
+    if not last_Candidate:
+        last_Candidate = Candidate
+
+    # if they're the same, log it
+    if Candidate == last_Candidate:
+        Vote_count += count
+    else:
+        # 
+        result = [last_Candidate, Vote_count]
+        if Vote_count > 1:
+           print("\t".join(str(v) for v in result))
+        last_Candidate = Candidate
+        Vote_count = 1
+
+# this is to catch the final value that we output
+print("\t".join(str(v) for v in [last_Candidate, Vote_count]))
